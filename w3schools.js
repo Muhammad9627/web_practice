2a0204decf4da0ba10423631b0e6e7aabc621d0b
Muhammad9627 <142158339+Muhammad9627@users.noreply.github.com>
2024-02-23 21:22:18 +0100

    Add files via upload

--- w3schools.js
+++ w3schools.js
@@ -41,9 +41,9 @@
 // (123).toString() + "<br>" +
 // (100 + 23).toString());
 // let x = 9.656;
-// console.log(x.toExponential() + "<br>" + 
-// x.toExponential(2) + "<br>" + 
-// x.toExponential(4) + "<br>" + 
+// console.log(x.toExponential() + "<br>" +
+// x.toExponential(2) + "<br>" +
+// x.toExponential(4) + "<br>" +
 // x.toExponential(6));
 // let x = 9.656;
 // console.log(  x.toFixed(0) + "<br>" +
@@ -61,11 +61,11 @@
 //     (100 + 23).valueOf());
 // console.log( Number(true) + "<br>" +
 // Number(false) + "<br>" +
-// Number("10") + "<br>" + 
+// Number("10") + "<br>" +
 // Number("  10") + "<br>" +
 // Number("10  ") + "<br>" +
 // Number(" 10  ") + "<br>" +
-// Number("10.33") + "<br>" + 
+// Number("10.33") + "<br>" +
 // Number("10,33") + "<br>" +
 // Number("10 33") + "<br>" +
 // Number("John"));
@@ -79,12 +79,12 @@
 // parseInt("-10.33") + "<br>" +
 // parseInt("10") + "<br>" +
 // parseInt("10.33") + "<br>" +
-// parseInt("10 6") + "<br>" +  
-// parseInt("10 years") + "<br>" +  
+// parseInt("10 6") + "<br>" +
+// parseInt("10 years") + "<br>" +
 // parseInt("years 10"));
 // console.log(parseFloat("10") + "<br>" +
 // parseFloat("10.33") + "<br>" +
-// parseFloat("10 6") + "<br>" +  
+// parseFloat("10 6") + "<br>" +
 // parseFloat("10 years") + "<br>" +
 // parseFloat("years 10"));
 // console.log(Number.isInteger(10) + "<br>" + Number.isInteger(10.5));
@@ -98,6 +98,594 @@
 // Number.parseInt("-10.33") + "<br>" +
 // Number.parseInt("10") + "<br>" +
 // Number.parseInt("10.33") + "<br>" +
-// Number.parseInt("10 6") + "<br>" +  
-// Number.parseInt("10 years") + "<br>" +  
+// Number.parseInt("10 6") + "<br>" +
+// Number.parseInt("10 years") + "<br>" +
 // Number.parseInt("years 10"));
+
+//sorting array
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits);
+// fruits.sort();
+// console.log(fruits);
+//sort works based on alphabetic order
+
+
+//looping an array
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fLen = fruits.length;
+
+// let text = "<ul>";
+// for (let i = 0; i < fLen; i++) {
+//   text += "<li>" + fruits[i] + "</li>";
+// }
+// text += "</ul>";
+// console.log(text);
+
+//array.for each
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let text = "<ul>";
+// fruits.forEach(myFunction);
+// text += "</ul>";
+// console.log(text);
+// function myFunction(value) {
+//     text += "<li>" + value + "</li>";
+//   }
+
+//adding element to array
+// const fruits = ["Banana", "Orange", "Apple"];
+// console.log(fruits);
+
+// function myFunction() {
+//   fruits[fruits.length] = "Lemon";
+//   console.log(fruits);
+// }
+
+//sorting numbers
+/**const points = [40, 100, 1, 5, 25, 10];
+console.log(points);  
+
+points.sort(function(a, b){return a - b}); 
+*/
+// const points = [40, 100, 1, 5, 25, 10];
+// function myFunction() {
+//     for (let i = points.length -1; i > 0; i--) {
+//       let j = Math.floor(Math.random() * (i+1));
+//       let k = points[i];
+//       points[i] = points[j];
+//       points[j] = k;
+//     }}console.log(points);
+
+// const points = [40, 100, 1, 5, 25, 10];
+// function myArrayMin(arr) {
+//     return Math.min.apply(null, arr);
+//   }
+//   console.log(points);
+
+// function myArrayMax(arr) {
+//     let len = arr.length;
+//     let max = -Infinity;
+//     while (len--) {
+//       if (arr[len] > max) {
+//         max = arr[len];
+//       }
+//     }
+//     return max;
+//   }
+  
+// const points = [40, 100, 1, 5, 25, 10];
+
+// const numbers = [45, 4, 9, 16, 25];
+// let txt = "";
+// numbers.forEach(myFunction);
+// function myFunction(value, index, array) {
+//     txt += value + "<br>"; 
+//   }
+
+// const numbers1 = [45, 4, 9, 16, 25];
+// const numbers2 = numbers1.map(myFunction);
+
+// document.getElementById("demo").innerHTML = numbers2;
+
+// function myFunction(value, index, array) {
+//   return value * 2;
+// }
+
+// const myArr = [1, 2, 3, 4, 5,6];
+// const newArr = myArr.flatMap((x) => x * 2);
+// document.getElementById("demo").innerHTML = newArr;
+
+// const numbers = [45, 4, 9, 16, 25];
+// const over18 = numbers.filter(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
+
+// const numbers = [45, 4, 9, 16, 25];
+// const over18 = numbers.filter(myFunction);
+
+// function myFunction(value) {
+//   return value > 18;
+// }
+
+// const numbers = [45, 4, 9, 16, 25];
+// let sum = numbers.reduceRight(myFunction);
+
+// document.getElementById("demo").innerHTML = "The sum is " + sum;
+
+// function myFunction(total, value, index, array) {
+//   return total + value;
+// }
+
+// const numbers = [45, 4, 9, 16, 25];
+// let allOver18 = numbers.every(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
+
+
+// function multiply(arr, n) {
+//     var product = 1;
+//     for (var i = 0; i < n; i++) {
+//         product *= arr[i];
+//     }
+//     console.log(product);
+//   }
+  
+//   multiply([1, 2, 3, 4, 5, 6], 3);
+
+// function duplicate(arr) {
+//     const doubleNum = []
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       doubleNum.push(2 * arr[i])
+//     }
+//     return doubleNum
+//   }
+  
+//   const arr = [1,2,3,4]
+//   console.log(duplicate(arr))
+  
+
+// function duplicate(arr) {
+//     let numDouble = arr;
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] != 0) {
+//             // Total = numDouble * 2;
+//             numDouble[i] = numDouble[i] * 2;
+//         }
+//     }
+//     return numDouble;
+// }
+
+// function duplicate(arr){
+//     let numDouble = arr;
+//     for(let i=0; i=arr.length; i++){
+//       if(arr!=0){
+//         numDouble[i]= numDouble[i] * 2; 
+//       }
+//     }
+//     return numDouble;
+//   }
+//   console.log(duplicate([1,2,3,4,5])); 
+
+//   const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const keys = fruits.keys();
+
+// for (let x of keys) {
+//   text += x + "<br>";
+// }
+
+// const name = "W3SCHOOLs";
+// let text ="";
+// for (const x of name){
+//     text += x + "-";
+// }
+// console.log(text);
+
+// for(const key in obj){
+//     //Check if keys are not inherited
+//     if(obj.hasOwnProperty(key)){
+//       console.log(obj[key]); 
+//     }
+//  }
+ 
+//  //"Prashant Yadav"
+//  //"26"
+//  //"Male"
+//  //function(){
+//  //  return `${this.name} is ${this.age} years old`;
+//  //}
+
+//javascript arrays
+const myArr = [1,2,3,4,5,6];
+const newArr = myArr.flatMap((x) => x*2);
+console.log(newArr);
+
+//array filter
+const numbers = [45,4,9,16,25];
+const over18 = numbers.filter(myFunction);
+function myFunction(value, index, array) {
+    return value > 18;
+}
+console.log(over18);
+//javascript arrays filter method
+const numbers = [45,4,2,9,16,25];
+const over18 = numbers.filter(myFunction);
+console.log(over18);
+function myFunction(value, index, array) {
+    return value % 2 == 0;
+}
+//javascript arrays filter method
+const numbers = [45,4,2,9,16,25];
+const over18 = numbers.filter(myFunction);
+console.log(over18);
+function myFunction(value) {
+    return value > 18;
+}
+
+//javascript arrays reduce method
+const numbers = [45,4,2,9,16,25];
+let sum = numbers.reduce(myFunction);
+console.log("The sum is " + sum);
+function myFunction(total, value, index, array) {
+    return  total + value;
+}
+
+//javascript arrays reduce method
+const numbers = [45,4,2,9,16,25];
+let sum = numbers.reduce(myFunction);
+console.log(sum);
+function myFunction(total, value) {
+    return  total + value;
+}
+
+//javascript arrays reduce method
+const numbers = [45,4,2,9,16,25];
+let sum = numbers.reduce(myFunction, 200);
+console.log("The sum is " + sum);
+function myFunction(total, value) {
+    return  total + value;
+}
+//javascript arrays reduce right method
+const numbers = [45,4,2,9,16,25];
+let sum = numbers.reduceRight(myFunction, );
+console.log("The sum is " + sum);
+function myFunction(total, value, index, array) {
+    return  total + value;
+}
+
+//javascript arrays reduce right method
+const numbers = [45,4,2,9,16,25];
+let sum = numbers.reduceRight(myFunction);
+console.log("The sum is " + sum);
+function myFunction(total, value) {
+    return  total + value;
+}
+
+//javascript arrays every method
+const numbers = [45,4,2,9,16,25];
+let allOver18 = numbers.every(myFunction);
+console.log(allOver18);
+function myFunction(total, value, array) {
+    return  value > 18;
+}
+
+//javascript arrays every method
+const numbers = [45,4,2,9,16,25];
+let allOver18 = numbers.every(myFunction);
+console.log(allOver18);
+function myFunction( value ) {
+    return  value > 18;
+}
+
+//javascript arrays every method
+const numbers = [45,4,2,9,16,25];
+let someOver18 = numbers.some(myFunction);
+console.log(allOver18);
+function myFunction( value, index, array ) {
+    return  value > 18;
+}
+
+//javascript from method
+const myArr = Array.from("ABCDEFG")
+console.log(myArr);
+
+//javascript keys method
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const keys = fruits.keys();
+let text ="";
+for (let x of keys){
+    text += x + "<br>";
+    console.log(keys);
+}
+
+//javascript array entries method
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const f = fruits.entries();
+for (let x of f){
+    console.log(+= x + "<br>");
+}
+
+//javascript arrays with method
+const months = ["januar", "Februar", "Mar", "April"];
+const myMonths = months.with(2,"March");
+console.log(myMonths);
+
+//javascript operators
+const q1 = ["Jan", "Feb", "Mar"];
+const q2 = ["Apr", "May", "Jun"];
+const q3 = ["Jul", "Aug", "Sep"];
+const q4 = ["Oct", "Nov", "Dec"];
+const year = [ ...q1, ...q2, ...q3, ...q4];
+console.log(year);
+
+//javascript arrays push method
+const cars = ["Saab", "Volvo", "BMW"];
+cars[0]= "Toyota";
+cars.push("Audi");
+//javascript arrays using const
+const cars = ["Saab", "Volvo", "BMW"];
+{
+    const cars = ["Toyota", "Volvo","BMW"];
+}
+console.log(cars[0]);
+//javascript dates
+const d = new Date();
+console.log(d);
+//new date (date string)
+const d = new Date("October 13, 2014 11:13:00");
+console.log(d);
+//new date  string
+const d = new Date("2022-03-25");
+console.log(d);
+//javascript dates
+const d = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(d);
+//javascript dates
+const d = new Date(2019,15,24,1,33,30);
+console.log(d);
+//javascript randon integers
+function getRndInteger(min,max) {
+    return Math.floor(Math.random()*(max - min))+min;  
+}
+//javascript booleans
+const x = new Boolean(false);
+const y = new Boolean(false);
+console.log(x===y);
+//javascript if 
+if (new Date().getHours() < 18){
+    console.log("Good day!");
+}
+
+//javascript if/elseif/else
+if(time < 10){
+    greeting = "Good Morning";
+}else if (time < 20){
+    greeting = "Good Day";
+}else {
+    greeting = "Good evening";
+}
+console.log(greeting);
+//for loop
+text += cars[0] + "<br>";
+text += cars[1] + "<br>";
+text += cars[2] + "<br>";
+text += cars[3] + "<br>";
+text += cars[4] + "<br>";
+text += cars[5] + "<br>";
+//or
+for (let i = 0; i < cars.length; i++){
+    text += cars[i] + "<br>";
+}
+//
+for (let i = 0; i < 5; i++){
+    text +="The number is" + i + "<br>";
+}
+
+//or
+const cars = ["BMW", "Volvo", "Saab","Ford"];
+let i , len, text;
+for (i = 0, len = cars.length, text = "";
+i < len; i++){
+    text += cars[i] + "<br>";
+}
+console.log(text);
+//or
+const person = {fname:"John",lname:"Doe",age:25};
+let txt = "";
+for (let x in person){txt += person[x] + "";
+}
+console.log(txt);
+//call back function
+const numbers = [45,4,9,16,25];
+let txt ="";
+numbers.forEach(myFunction);
+function myFunction(value, index, array) {
+    txt += value;
+}
+//do while loop
+let i = 0;
+do {
+    text += "The number is " + i;
+    i++;
+}
+while (i < 10);
+//
+const cars = ["BMW","Volvo","Saab", "Ford"];
+let i = 0;
+let text = "";
+for (;cars[i];){
+    text += cars[i];
+    i++;
+}
+//
+const cars = ["BMW","Volvo","Saab", "Ford"];
+let i = 0;
+let text = "";
+while (cars[i]){
+    text += cars[i];
+    i++;
+}
+//break
+for (let i = 0; i< 10; i++){
+    if (i === 3) {break;}
+    text += "The number is " + i + "<br>";
+}
+//continue
+for (let i = 0; i< 10; i++){
+    if (i === 3) {continue;}
+    text += "The number is " + i + "<br>";}
+
+    //iterable
+    const letters = new set (["a","b","c"]);
+    let text = "";
+    for (const x of letters){
+        text += x + "<br>"
+    }
+    console.log(text);
+    //
+    const name = "W3Schools";
+    let text = "";
+    for (const x of name){
+        text += x + "<br>"
+    }
+    console.log(text);
+    //
+    const fruits = new Map([
+        ["apples",500],
+        ["bananas",300]
+        ["oranges",200]
+    ]);
+    let text = "";
+    for (const x of fruits){
+        text += x "br";
+    }
+    console.log(text);
+    //javascript array set
+    const letters = new set(["a," "b," "c"]);
+    console.log(letter.size);
+    //
+    const letters = new set();
+    const a ="a";
+    const b ="b";
+    const c ="c";
+    const d ="d";
+
+    letters.add("a");
+    letters.add("b");
+    letters.add("c");
+    letters.add("d");
+    console.log(letters.size);
+    //
+    const letter = new set(["a","b","c"]);
+    letters.add("d");
+    letters.add("e");
+    console.log(letters.size);
+    //for each method
+    const letters = new set(["a","b","c"]);
+    let text = "";
+    letters.forEach(function (value) {
+        text += value;
+    })
+    console.log(text);
+    //array object
+    const fruits =["banana","orange","apple","mango"];
+    console.log(isArray(fruits));
+    function isArray(myArray) {
+        return myArray.constructor === Array;
+    }
+    myFunction();
+    console.log("I can display" + carName);
+    function myFunction() {
+        carName = "volvo";
+    }
+    // 
+    var x = 5;
+    elem = console.log("x is " + x + "and y is "+ y);
+    var y = 7;
+    //this
+    const person = {
+        firstName: "John",
+        lastName: "Doe",
+        id: 5566,
+        fullName : function () {
+            return this.firstName + "" + this.lastName;
+        };
+        console.log(person.fullName());
+    }
+    //
+    const person1 = {
+        fullName: function () {
+            return this.firstName + "" + this.lastName;
+            }
+    }
+    const person2 = {
+        firstName:"John",
+        lastName:"Doe",
+    }
+    let x = person1.fullName.call(person2);
+    console.log(x);
+    //
+    const person = {
+        fullName: function () {
+            return this.firstName + "" + this.lastName;
+            }
+    }
+    const member = {
+        firstName:"Hege",
+        lastName:"Nilsen",
+    }
+    let fullName = person.fullName.bind(member);
+    console.log(fullName);
+    //class syntax
+    class car {
+        constructor(name, year){
+    this.name = name;
+    this.year = year;
+    }
+age(){
+    const date = new Date();
+    return date.getFullYear() - this.year;
+}
+}
+const myCar = new car("Ford",2014);
+console.log("My car is"+ myCar.age() + "year old.");
+json parse(text)
+let text = '{"employees":[' + '{"firstName":"John","lastName":"Doe"},' + '{"firstName":"Anna","lastName":"Smith"},' + '{"firstName":"Peter","lastName":"Jones"}]}';
+const obj = JSON.parse(text);
+console.log(obj.employees[1].firstName + " " + obj.employees[1].lastName); 
+//code indentation
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit - 32);
+}
+//switch case
+let day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+    default:
+        day = "unknown";
+}
+console.log("Today is " + day);
