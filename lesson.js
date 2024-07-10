9a8b3aa2ac12db7d87d9f25eaca4ffc9419b8624
Muhammad9627 <142158339+Muhammad9627@users.noreply.github.com>
2024-07-10 22:56:12 +0100

    Add files via upload

---
+++ lesson.js
@@ -0,0 +1,48 @@
+//successfull function that print array element that are divisible by two without remainder
+
+// function check(arr) {
+//     let newArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         let element = (arr[i]);
+//         if (element % 2 == 0) {
+//             newArr.push(element);
+//         }
+//     }
+//     console.log(newArr);
+
+// }
+// check(arr = [2, 3, 4, 5, 6, 50, 100, 1000]);
+//
+//function check(arr) {
+//     let newArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         let element = (arr[i]);
+//         if (element % 2 == 0) {
+//         element**2
+//         newArr.push(element);
+//         }
+//     }
+//     console.log(newArr);
+
+// }
+// check(arr = [2, 3, 4, 5, 6, 50, 100, 1000]);
+
+//javascript function that takes an array of objects data as input and return a new array with the following transformation applied:each object in the array should have it price property rounded to two places. the category property should be converted to uppercase. a new property tax should be added, calculated as 10% of the rounded price.the objects in the array should be sorted in the descending order based on the price property
+//partly success
+
+// let array1 = [];
+// function input(array) {
+//     for (let i = 0; i < data.length; i++) {
+//         data[i].price = (data[i].price).toFixed(2);
+//         data[i].category = (data[i].category).toUpperCase();
+//         data[i].Tax = (data[i].price / 10).toFixed(2);
+//         array1.push(data[i]);
+//     } //console.log(data);
+//     console.log(array1);
+// }
+// input(data =
+//     [
+//         { id: 1, price: 12.345, category: 'electronics' },
+//         { id: 2, price: 23.456, category: 'clothing' },
+//         { id: 3, price: 34.567, category: 'groceries' }
+//     ]);
